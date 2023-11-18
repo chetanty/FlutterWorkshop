f6bdb8ff79e4eebcecc4cd567fcdf0dd67bc6ef3
Chetan Tyagi <87710619+chetanty@users.noreply.github.com>
2023-11-18 05:48:46 -0700

    Create navigation.dart

---
+++ navigation.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/', // Set the initial route.
+      routes: {
+        '/': (context) => MyHomePage(),
+        '/second': (context) => SecondScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the 'second' route when the button is pressed.
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text('Go to Second Screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to the 'home' route when the button is pressed.
+            Navigator.pop(context);
+          },
+          child: Text('Go back to Home Screen'),
+        ),
+      ),
+    );
+  }
+}
